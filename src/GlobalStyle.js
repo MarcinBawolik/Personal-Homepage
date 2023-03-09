1ee2c1da8a3533c336f8279ef819a4d6bca629d8
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-09 10:01:17 +0100

    added GlobalStyles to application

---
+++ src/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*, ::after, ::before {
+  box-sizing: inherit;
+}
+
+body {
+  background-color: rgb(230, 227, 227);
+  word-break: break-word;
+}
+`;
