a17579227ca33547f6c159801ad79a76c6cde4ee
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-14 14:28:04 +0100

    added discription

--- src/Informations/index.js
+++ src/Informations/index.js
@@ -1,15 +1,19 @@
 import { Wrapper, Image, ParagraphWrapper, Paragraph } from "./styled";
 
 const Informations = () => (
-    <Wrapper>
-        <Image />
-        <ParagraphWrapper>
-            <Paragraph this>This is</Paragraph>
-            <Paragraph name>Marcin Bawolik</Paragraph>
-            <Paragraph discription>🧑🏻💻I'm a passionate Frontend Developer in love with React, currently looking for new job opportunites.</Paragraph>
-            <Paragraph hire></Paragraph>
-        </ParagraphWrapper>
-    </Wrapper>
-)
+  <Wrapper>
+    <Image />
+    <ParagraphWrapper>
+      <Paragraph toggle></Paragraph>
+      <Paragraph this>This is</Paragraph>
+      <Paragraph name>Marcin Bawolik</Paragraph>
+      <Paragraph discription>
+        🧑🏻💻I am a young, ambitious person. I love challenges, teamwork and
+        solving problems. I am currently looking for new job opportunities.
+      </Paragraph>
+      <Paragraph hire></Paragraph>
+    </ParagraphWrapper>
+  </Wrapper>
+);
 
 export default Informations;
