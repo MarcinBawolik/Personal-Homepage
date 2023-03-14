1c2fa2589b79ebfbb6e1c5ee27e1419609b6a5c8
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-14 14:28:59 +0100

    added LiWrapper and LiBullet to SectionList

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,69 +1,135 @@
 import {
-    SectionDivider,
-    SectionHeader,
-    SectionList,
-    SectionListItem,
-    SectionSkillWrapper,
-    StyledSection
+  SectionDivider,
+  SectionHeader,
+  SectionList,
+  SectionListItem as SectionLi,
+  LiBullet,
+  LiWrapper,
+  SectionSkillWrapper,
+  StyledSection,
 } from "./style";
 
-
 export const Section = ({ title }) => (
-    <>
-        <StyledSection>
-            <SectionHeader>{title}</SectionHeader>
-            <SectionDivider />
-            <SectionSkillWrapper>
-                <SectionList>
-                    <SectionListItem>Semantic & accessibe HTML</SectionListItem>
-                    <SectionListItem>Responsive Web Design</SectionListItem>
-                    <SectionListItem>TeamWork</SectionListItem>
-                    <SectionListItem>Markdown</SectionListItem>
-                    <SectionListItem>Immutabilty</SectionListItem>
-                    <SectionListItem>CSS BEM convention</SectionListItem>
-                    <SectionListItem>CSS Grid</SectionListItem>
-                    <SectionListItem>CSS Flexbox</SectionListItem>
-                </SectionList>
-                <SectionList>
-                    <SectionListItem>React Router</SectionListItem>
-                    <SectionListItem>Redux-Saga</SectionListItem>
-                    <SectionListItem>Redux (toolkit)</SectionListItem>
-                    <SectionListItem>React Hooks</SectionListItem>
-                    <SectionListItem>Error handling</SectionListItem>
-                    <SectionListItem>Working with API (fetch, axios)</SectionListItem>
-                    <SectionListItem>JavaScript: ES6+</SectionListItem>
-                </SectionList>
-                <SectionList>
-                    <SectionListItem>Promises, Async/Await</SectionListItem>
-                    <SectionListItem>GitHub Pull Requests & Review</SectionListItem>
-                    <SectionListItem>Trello</SectionListItem>
-                    <SectionListItem>Scrum</SectionListItem>
-                    <SectionListItem>NPM</SectionListItem>
-                    <SectionListItem>React</SectionListItem>
-                    <SectionListItem>Git</SectionListItem>
-                </SectionList>
-            </SectionSkillWrapper>
-        </StyledSection>
-    </>
-)
+  <>
+    <StyledSection>
+      <SectionHeader>{title}</SectionHeader>
+      <SectionDivider />
+      <SectionSkillWrapper>
+        <SectionList>
+          <LiWrapper>
+            <LiBullet />
+            <SectionLi>Semantic & accessibe HTML</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet />
+            <SectionLi>Responsive Web Design</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet />
+            <SectionLi>TeamWork</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>Markdown</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>Immutabilty</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>CSS BEM convention</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>CSS Grid</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>CSS Flexbox</SectionLi>
+          </LiWrapper>
+        </SectionList>
+        <SectionList>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>React Router</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>Redux-Saga</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>Redux (toolkit)</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>React Hooks</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>Error handling</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>Working with API (fetch, axios)</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>JavaScript: ES6+</SectionLi>
+          </LiWrapper>
+        </SectionList>
+        <SectionList>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>Promises, Async/Await</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>GitHub Pull Requests & Review</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>Trello</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>Scrum</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>NPM</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>React</SectionLi>
+          </LiWrapper>
+          <LiWrapper>
+            <LiBullet /> <SectionLi>Git</SectionLi>
+          </LiWrapper>
+        </SectionList>
+      </SectionSkillWrapper>
+    </StyledSection>
+  </>
+);
 
 export const ShortSection = ({ title }) => (
-    <StyledSection>
-        <SectionHeader>{title}</SectionHeader>
-        <SectionDivider />
-        <SectionSkillWrapper>
-            <SectionList>
-                <SectionListItem>React Context</SectionListItem>
-                <SectionListItem>Node.js</SectionListItem>
-            </SectionList>
-            <SectionList>
-                <SectionListItem>Cypress</SectionListItem>
-                <SectionListItem>Unit testing</SectionListItem>
-            </SectionList>
-            <SectionList>
-                <SectionListItem>TypeScript</SectionListItem>
-                <SectionListItem>JS classes</SectionListItem>
-            </SectionList>
-        </SectionSkillWrapper>
-    </StyledSection>
-)
+  <StyledSection>
+    <SectionHeader>{title}</SectionHeader>
+    <SectionDivider />
+    <SectionSkillWrapper>
+      <SectionList>
+        <LiWrapper>
+          <LiBullet />
+          <SectionLi>React Context</SectionLi>
+        </LiWrapper>
+        <LiWrapper>
+          <LiBullet />
+          <SectionLi>Node.js</SectionLi>
+        </LiWrapper>
+      </SectionList>
+      <SectionList>
+        <LiWrapper>
+          <LiBullet />
+          <SectionLi>Cypress</SectionLi>
+        </LiWrapper>
+        <LiWrapper>
+          <LiBullet />
+          <SectionLi>Unit testing</SectionLi>
+        </LiWrapper>
+      </SectionList>
+      <SectionList>
+        <LiWrapper>
+          <LiBullet />
+          <SectionLi>TypeScript</SectionLi>
+        </LiWrapper>
+        <LiWrapper>
+          <LiBullet />
+          <SectionLi>JS classes</SectionLi>
+        </LiWrapper>
+      </SectionList>
+    </SectionSkillWrapper>
+  </StyledSection>
+);
