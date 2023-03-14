71a37f90c685562fef8a9262e9fa6b89d7302fa0
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-14 14:21:57 +0100

    chaned content and added linkedin icon

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -1,11 +1,12 @@
 import { Image, Paragraph, Wrapper } from "./styled";
-import git from "../images/git.png"
+import github_black from "../images/github_black.svg"
+import linkedin_black from "../images/linkedin_black.svg"
 
 export const Contact = () => (
     <Wrapper>
 <Paragraph talk>LET'S TALK</Paragraph>
 <Paragraph email>martinbawolik@gmail.com</Paragraph>
-<Paragraph> I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come<br /> to life, feel free to conatct me</Paragraph>
-<Paragraph><Image src={git} alt=""/></Paragraph>
+<Paragraph content> I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come<br /> to life, feel free to conatct me</Paragraph>
+<Paragraph><Image src={github_black} alt=""/><Image src={linkedin_black} alt=""/></Paragraph>
     </Wrapper>
 )
