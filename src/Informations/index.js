f3fbe79f38a3f4d0f4a2d9619cbf0be7638f6c39
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-17 17:55:10 +0100

    added ToggleButton

--- src/Informations/index.js
+++ src/Informations/index.js
@@ -1,20 +1,48 @@
-import { Wrapper, Image, ParagraphWrapper, Paragraph } from "./styled";
-import me from "../images/me.jpg"
+import {
+  Wrapper,
+  Image,
+  ParagraphWrapper,
+  ToggleButton,
+  Text,
+  IconBox,
+  SunIcon,
+  ToggleButtonWrapper,
+  HireButton,
+  Header,
+  Discription,
+  ThisIs,
+} from "./styled";
+import me from "../images/me.jpg";
+import { useState } from "react";
 
-const Informations = () => (
-  <Wrapper>
-    <Image src={me} alt=""/>
-    <ParagraphWrapper>
-      <Paragraph toggle></Paragraph>
-      <Paragraph this>This is</Paragraph>
-      <Paragraph name>Marcin Bawolik</Paragraph>
-      <Paragraph discription>
-        🧑🏻💻I am a young, ambitious person. I love challenges, teamwork and
-        solving problems. I am currently looking for new job opportunities.
-      </Paragraph>
-      <Paragraph hire></Paragraph>
-    </ParagraphWrapper>
-  </Wrapper>
-);
+const Informations = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  function handleClick() {
+    setIsClicked(isClicked === false ? true : false);
+  }
+  return (
+    <Wrapper>
+      <Image src={me} alt="" />
+      <ParagraphWrapper>
+        <ToggleButtonWrapper>
+          <ToggleButton onClick={handleClick}>
+            <Text>Dark Mode {isClicked === true ? "On" : "Off"}</Text>
+            <IconBox>
+              <SunIcon isClicked={isClicked} />
+            </IconBox>
+          </ToggleButton>
+        </ToggleButtonWrapper>
+        <ThisIs>This is</ThisIs>
+        <Header>Marcin Bawolik</Header>
+        <Discription>
+          🧑🏻💻I am a young, ambitious person. I love challenges, teamwork and
+          solving problems. I am currently looking for new job opportunities.
+        </Discription>
+        <HireButton />
+      </ParagraphWrapper>
+    </Wrapper>
+  );
+};
 
 export default Informations;
