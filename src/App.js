b019a1da8be8b4b1dac76dd75cdb9707e8b6da9a
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-17 17:49:27 +0100

    deleted empty space

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import { Wrapper } from "./GlobalStyle";
 import Informations from "./Informations";
 import { Section, ShortSection } from "./Section";
 import { Tiles } from "./GitHubProjects/Tile";
+
 function App() {
   return (
-<>
-<Wrapper>
-<Informations />
-<Section
-      title="My skillset includes 🛠️"
-    />
-    <ShortSection  
-    title= "What i want to learn 🚀"
-    />
-    <Tiles />
-    <Contact />
-</Wrapper>
-</>
+    <Wrapper>
+      <Informations />
+      <Section title="My skillset includes 🛠️" />
+      <ShortSection title="What i want to learn next 🚀" />
+      <Tiles />
+      <Contact />
+    </Wrapper>
   );
 }
 
