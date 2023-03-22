23ec5527d1bc203476481896b2689a356796f5b6
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-22 16:29:27 +0100

    getting repo function

--- src/getRepo.js
+++ src/getRepo.js
@@ -1,4 +1,5 @@
-export async function getRepositories({ username }) {
+export async function getRepositories() {
+    const username = 'MarcinBawolik';
     const url = `https://api.github.com/users/${username}/repos`;
     const response = await fetch(url);
     const data = await response.json();
