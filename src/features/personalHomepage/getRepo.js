edbebdcfb9bbd254b46db8f65dd58059ec5f9b13
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-04-06 16:40:10 +0200

    moved to feature personalHomepage

---
+++ src/features/personalHomepage/getRepo.js
@@ -0,0 +1,17 @@
+export async function getRepositories() {
+  const username = "MarcinBawolik";
+  const url = `https://api.github.com/users/${username}/repos`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
--- src/getRepo.js
+++
@@ -1,7 +0,0 @@
-export async function getRepositories() {
-  const username = "MarcinBawolik";
-  const url = `https://api.github.com/users/${username}/repos`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-}
